76e6cd64716b840041b7bf9be1bfe13f0a0a5073
Rahmad <163022948+linndend@users.noreply.github.com>
2025-04-30 14:38:23 +0700

    Add files via upload

---
+++ TriggerX.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+clear
+
+center_text() {
+  local text="$1"
+  local width=$(tput cols)
+  local padding=$(( (width - ${#text}) / 2 ))
+  printf "%*s%s\n" $padding "" "$text"
+}
+
+line=$(printf '=%.0s' $(seq 1 $(tput cols)))
+
+echo "$line"
+center_text "Setup TriggerX Node"
+echo "$line"
+
+clear
+
+echo "============================================="
+echo        "INSTALL DEPENDENCIES & ENV"
+echo "============================================="
+
+sudo apt update && sudo apt upgrade -y
+sleep 3
+sudo apt install git nodejs npm curl
+sleep
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash 
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+nvm i --lts
+sleep 3
+
+echo "============================================="
+echo        "Setup NODE"
+echo "============================================="
+
+git clone https://github.com/trigg3rX/triggerx-keeper-setup.git
+sleep 3
+cd triggerx-keeper-setup
+sleep 2
+cp .env.example .env
+nano .env
+sleep 5
+
+npm i -g @othentic/othentic-cli@1.10.0
+sleep 3
+othenthic-cli init
+sleep 3
+othenthic-cli operator register
+sleep 2
+chmod +x ./triggerx.sh
+./triggerx.sh start 
+sleep 3
+./triggerx.sh start-mon
+
+echo "✅ Setup complete!"
